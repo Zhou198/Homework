3606a7e19b2fe33335e571e7d308a71903bfa32c
Zhou198 <47863455+Zhou198@users.noreply.github.com>
2020-02-16 21:30:46 -0500

    Update 557.R

--- 557.R
+++ 557.R
@@ -1,265 +1,93 @@
-################  Homework1 01/23/2020  #################
-#### T3 ####
-T <- 10000; n <- 25; SigX <- 2; SigY <- 3; gam <- (SigY/SigX)^2
-set.seed(1)
-hatgam <- sapply(1:T, function(t) {
-  X <- rnorm(n, 0, SigX); Y <- rnorm(n, 0, SigY)
-  var(Y)/var(X)
-})
-CI <- hatgam %*% matrix(qf(c(0.025, 0.975), 24, 24), ncol = 2)
-
-mean(CI[, 1] <= gam & gam <= CI[, 2])
-
-############  ———————— ###############
-################  Homework2 01/25/2020  #################
-#### T3 ####
-### Method 1 ###
-########### Randomn Number Generator #############
-rX <- function(n){
-  u <- runif(n)
-  ifelse(u < 1/3, 3 * u, 1.5 * u + 0.5)
-}
-
-## sample mean
-N <- 5000; Y <- 1
-set.seed(1)
-#tilMu1 <- sum(X * (X <= Y))/sum(X <= Y)
-tilMu1 <- sapply(10:N, function(n){
-  X <- rX(n); sum(X * (X <= Y))/sum(X <= Y)
-})
-
-par(mai = c(0.83, 0.8, 0.2, 0.1)) ## 600 x 420
-plot(10:N, tilMu1, type = "l", col = "cyan3", xlab = "Sample Size", ylab = expression(tilde(mu)))
-abline(h = 1/2, col = "tomato3")
-
-## modified edf
-par(mai = c(0.83, 0.88, 0.2, 0.1))
-x <- seq(-1, 3, length = 2000)
-Fx <- x/3 * (0 < x & x <= 1) + (2 * x - 1)/3 * (1 < x & x <= 2) + (x > 2)
-plot(x, Fx, type = "l", ylab = expression(tilde(F)(x)))
-
-
-tilF1 <- function(x){
-  sum(X <= Y & X <= x)/sum(X <= Y)
-}
-
-tilP1 <- sapply(x, function(t) tilF1(t))
-points(x, tilP1, type = "l")
-
-sam <- c(5000, 1000, 100, 50, 20)
-col <- c("tomato3", "green3", "cyan3", "darkorchid3", "burlywood3")
-set.seed(1)
-for (i in 1:5) {
-   X <- rX(sam[i]); Y <- 1
-   tilP1 <- sapply(x, function(t) tilF1(t))
-   points(x, tilP1, type = "l", col = col[i])
-}
-
-legend("topleft", legend = c("N = 5000", "N = 1000", "N = 100", "N = 50", "N = 20", "Exact CDF"), bty = "n", lty = rep(1, 6), col = c("tomato3", "green3", "cyan3", "darkorchid3", "burlywood3", "black"), text.col = c("tomato3", "green3", "cyan3", "darkorchid3", "burlywood3", "black"))
-
-### Method 2 ###
-## sample mean
-#tilMu2 <- mean(X * (X <= Y) + Y * (X > Y))
-set.seed(1)
-tilMu2 <- sapply(10:N, function(n) {
-  X <- rX(n); mean(pmin(X, Y))
-})
-
-plot(10:N, tilMu2, type = "l", col = "cyan3", xlab = "Sample Size", ylab = expression(tilde(mu)))
-abline(h = 5/6, col = "tomato3")
-
-## modified edf
-tilF2 <- function(x) mean(pmin(X, Y) <= x) 
-
-x <- seq(-1, 3, length = 2000)
-Fx <- x/3 * (0 < x & x <= 1) + (2 * x - 1)/3 * (1 < x & x <= 2) + (x > 2)
-plot(x, Fx, type = "l", ylab = expression(tilde(F)(x)))
-
-set.seed(1)
-for (i in 1:5) {
-  X <- rX(sam[i]); Y <- 1
-  tilP2 <- sapply(x, function(t) tilF2(t))
-  points(x, tilP2, type = "l", col = col[i])
-}
-
-legend("topleft", legend = c("N = 5000", "N = 1000", "N = 100", "N = 50", "N = 20", "Exact CDF"), bty = "n", lty = rep(1, 6), col = c("tomato3", "green3", "cyan3", "darkorchid3", "burlywood3", "black"), text.col = c("tomato3", "green3", "cyan3", "darkorchid3", "burlywood3", "black"))
-
-
-## change y into 1.5
-x <- seq(-1, 3, length = 2000)
-Fx <- x/3 * (0 < x & x <= 1) + (2 * x - 1)/3 * (1 < x & x <= 2) + (x > 2)
-plot(x, Fx, type = "l", ylab = expression(tilde(F)(x)))
-
-
-set.seed(2)
-for (i in 1:5) {
-  X <- rX(sam[i]); Y <- 1.5
-  tilP2 <- sapply(x, function(t) tilF2(t))
-  points(x, tilP2, type = "l", col = col[i])
-}
-
-legend("topleft", legend = c("N = 5000", "N = 1000", "N = 100", "N = 50", "N = 20", "Exact CDF"), bty = "n", lty = rep(1, 6), col = c("tomato3", "green3", "cyan3", "darkorchid3", "burlywood3", "black"), text.col = c("tomato3", "green3", "cyan3", "darkorchid3", "burlywood3", "black"))
-
-
-
-############  ———————— ###############
-################  Homework3 01/29/2020  #################
-library(ggplot2)
+################  Homework5 02/14/2020  #################
+library(splines)
+library(survival)
 
 #### T1 ####
-rN <- function(n, p){
-  Intvl <- cumsum(c(0, p))
-  sapply(runif(n), function(u) sum(Intvl <= u))
-} # p is a preset prob. vector for R.V. N
-
-
-Time <- 100; p <- c(0.1, 0.2, 0.05, 0.2, 0.15, 0.15, 0.1, 0.05)
-
 set.seed(1)
-X <- rexp(Time, 1/3); N <- rN(Time, p)
-
-XStar <- sapply(1:Time, function(i){
-  Y <- runif(N[i], 0, 2) + 2 * (1:N[i])
-  if (Y[1] < X[i] & X[i] <= Y[N[i]]) {k <- sum(Y < X[i]); 0.5 * (Y[k] + Y[k + 1])}
-  else Y[1] * (X[i] <= Y[1]) + Y[N[i]] * (X[i] > Y[N[i]])
-})
-
-range(XStar)
-x <- seq(min(XStar) - 5, max(XStar) + 5, length = 500)
-cdf <- sapply(x, function(s) mean(XStar <= s))
-excdf <- sapply(x, function(s) mean(X <= s))
-
-par(mai = c(0.83, 0.8, 0.2, 0.1))
-#plot(x, cdf, type = "l")
-
-datCDF <- cbind(x = x, prob. = cdf, exCDF = excdf)
-ggplot(data.frame(datCDF), aes(x = x)) + geom_line(aes(y = prob.), col = "tomato3") + geom_line(aes(y = exCDF), col = "cyan3") + theme(legend.position = c(0.1, 0.86))  + theme(legend.title = element_blank())
-
-                
-
-                
-
-################ Revise #################                
-Ftil <- function(p, Size, t, Time) {
-  sapply(1:Time, function(Ti) {
-    X <- rexp(Size, 1/3); N <- rN(Size, p)
-    
-    XStar <- sapply(1:Size, function(i){
-      Y <- runif(N[i], 0, 2) + 2 * (1:N[i])
-      if (Y[1] < X[i] & X[i] <= Y[N[i]]) {k <- sum(Y < X[i]); 0.5 * (Y[k] + Y[k + 1])}
-      else Y[1] * (X[i] <= Y[1]) + Y[N[i]] * (X[i] > Y[N[i]])
-    })
-    
-    mean(XStar <= t)
-  })
-}
-
-1 - exp(-1/3 * 2.5)
-
-
-set.seed(1)
-F3 <- Ftil(p, 100, 2.5, 10)
-plot(F3)
-
-
-set.seed(1)
-F3asy <- sapply(1:5000, function(i) Ftil(p, i, 3, 1))
-plot(1:5000, F3asy, type = "l")
-abline(h = 0.34)
-
-set.seed(1)
-F2.5asy <- sapply(1:5000, function(i) Ftil(p, i, 2.5, 1))
-plot(1:5000, F2.5asy, type = "l", col = "tomato3")
-
-par(mai = c(0.83, 0.8, 0.2, 0.1))
-#plot(x, cdf, type = "l")
-
-datCDF <- cbind(x = x, prob. = cdf, hatCDF = hatcdf, exCDF = excdf)
-ggplot(data.frame(datCDF), aes(x = x)) + 
-  geom_line(aes(y = prob.), col = "tomato3") + 
-  geom_line(aes(y = hatCDF), col = "black") +
-  geom_line(aes(y = exCDF), col = "cyan3") + 
+Y <- rweibull(100, 2, 1)  
+U <- runif(100)
+M <- Y * (Y <= U) + U * (Y > U)
   
 
 
-                
-                
-                
 #### T2 ####
 set.seed(1)
-U <- runif(400, 0, 100); V <- U + 40
-Tf <- rexp(400, 1/56); ind <- which(Tf <= V)
-TStar <- (U * (Tf <= U) + 0.5 * (U + V) * (U < Tf & Tf <= V))[ind]
+Y <- rweibull(100, 0.5, 1)
+U <- runif(100, 0, 2)
+d <- as.numeric(Y <= U)
+M <- Y * d + U * (1 - d)
 
-x <- seq(min(Tf) - 40, max(Tf) + 20, length = 1000)
-S <- (x <= 0) + exp(-1/56 * x) * (x > 0)
-Stil <- sapply(x, function(t) mean(TStar > t))
-
-datS <- cbind(x = x, exS = S, Stil = Stil)
-ggplot(data.frame(datS), aes(x = x)) + geom_line(aes(y = exS, col = "tomato3")) + geom_line(aes(y = Stil, col = "cyan3")) + labs(x = "x", y = "Probability") + theme(legend.background = element_blank(), legend.title = element_blank(), legend.position = c(0.93, 0.9)) + scale_color_manual(values = c("tomato3" = "tomato3", "cyan3" = "cyan3"), breaks = c("tomato3", "cyan3"), labels = c("S(x)", expression(tilde(S)(x))))
+model <- survreg(Surv(M, d) ~ 1)
+val <- summary(model)
+c(val$coef, sigma = val$scale)
 
 
+der2 <- function(ka, ta, x, d) {
+  xdt <- x/ta
+  matrix(c(-sum(d)/(ka^2) - sum(xdt^ka * (log(xdt))^2),
+           -sum(d)/ta + sum(xdt^ka/ta * (1 + ka * log(xdt))),
+           -sum(d)/ta + sum(xdt^ka/ta * (1 + ka * log(xdt))),
+           ka * sum(d)/(ta^2) - (ka^2 + ka)/(ta^2) * sum(xdt^ka)), nrow = 2)
+}
 
-genetic_code <- function(codon) {
-  if(str_detect(codon, "UU[UC]")) return("Phe")
-  if(str_detect(codon, "UU[AG]") | str_detect(codon, "CU[UCAG]")) return("Leu")
-  if(str_detect(codon, "AU[UCA]")) return("lle")
-  if(str_detect(codon, "AUG")) return("Met")
-  if(str_detect(codon, "GU[UCAG]")) return("Val")
+der1 <- function(ka, ta, x, d) {
+  xdt <- x/ta
+  matrix(c((1/ka - log(ta)) * sum(d) + sum(d * log(x)) - sum(xdt^ka * log(xdt)),
+           -ka/ta * sum(d) + ka/ta * sum(xdt^ka)), ncol = 1)
+}
+
+KT <- c(1, 2); S <- c(0, 0); 
+while (max(abs(KT - S)) >= 0.00001) {
+  S <- KT; inv <- solve(der2(KT[1], KT[2], M, d), diag(2))
+  KT <- KT - inv %*% der1(KT[1], KT[2], M, d)
+}
+
+c(alpha = log(KT[2]), logsigma = -log(KT[1]), sigma = 1/KT[1])
+
+KT
+
+
+####### T3 #######
+val$var
+
+A <- matrix(c(0, 1/KT[2], -1/KT[1], 0), nrow = 2)
+t(A) %*% -solve(der2(KT[1], KT[2], M, d), diag(2)) %*% A
+
+
+
+#### T4 ####
+vrho <- t(c(0, -1/(KT[2]^2))) %*% -solve(der2(KT[1], KT[2], M, d), diag(2)) %*% c(0, -1/(KT[2]^2))
+1/KT[2] + c(-1, 1) * qnorm(0.975) * as.vector(sqrt(vrho))
+
+
+
+#### T5 ####
+set.seed(1)
+Y <- rweibull(100, 2, 1); U <- runif(100)
+d <- as.numeric(Y <= U) 
+M <- Y * d + U * (1 - d)
+
+model5 <-  survreg(Surv(M, d) ~ 1)
+est <- data.frame(ka1 = 1/model5$scale, ta1 = exp(model5$coef), ta0 = mean(M)/mean(d))
+
+
+survreg(Surv(M, d) ~ 1, dist = "exponential")
+
+
+2 * ((log(est[1] * est[3]) - est[1] * log(est[2])) * sum(d) + (est[1] - 1) * sum(d * log(M)) + sum(M)/est[3] - est[2]^(-est[1]) * sum(M^est[1]))
+
+qchisq(0.95, df = 1)
+
   
-  if(str_detect(codon, "UC[UCAG]") | str_detect(codon, "AG[UC]")) return("Ser")
-  if(str_detect(codon, "CC[UCAG]")) return("Pro")
-  if(str_detect(codon, "AC[UCAG]")) return("Thr")
-  if(str_detect(codon, "GC[UCAG]")) return("Ala")
-  
-  if(str_detect(codon, "UA[UC]")) return("Tyr")
-  if(str_detect(codon, "UA[AG]") | str_detect(codon, "UGA")) return("Stop")
-  if(str_detect(codon, "CA[UC]")) return("His")
-  if(str_detect(codon, "CA[AG]")) return("Gln")
-  if(str_detect(codon, "AA[UC]")) return("Asn")
-  if(str_detect(codon, "AA[AG]")) return("Lys")
-  if(str_detect(codon, "GA[UC]")) return("Asp")
-  if(str_detect(codon, "GA[AG]")) return("Glu")
-  
 
-  if(str_detect(codon, "UG[UC]")) return("Cys")
-  if(str_detect(codon, "UGG")) return("Trp")
-  if(str_detect(codon, "CG[UCAG]") | str_detect(codon, "AG[AG]")) return("Arg")
-  if(str_detect(codon, "GG[UCAG]")) return("Gly")
-  }
+#### T7 ####
+set.seed(1)
+Y <- rweibull(4, 2, 1); U <- runif(4)
+d <- as.numeric(Y <= U) 
+M <- Y * d + U * (1 - d)
 
+model5 <-  survreg(Surv(M, d) ~ 1)
+est <- data.frame(ka1 = 1/model5$scale, ta1 = exp(model5$coef), ta0 = mean(M)/mean(d))
 
-
-codon <- str_sub(mRNA, seq(1, str_length(mRNA), by = 3), seq(3, str_length(mRNA), by = 3))
-amino <- sapply(codon, function(k) genetic_code(k))
-
-
-protein <- str_split(str_c(amino, "-", collapse = ""), "-Stop")[[1]][1]
-
-str_count(protein, "-") + 1
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+2 * ((log(est[1] * est[3]) - est[1] * log(est[2])) * sum(d) + (est[1] - 1) * sum(d * log(M)) + sum(M)/est[3] - est[2]^(-est[1]) * sum(M^est[1]))
 
