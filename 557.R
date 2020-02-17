6eb3de3093b308c8716e3701fa58c76ae31a6e30
Zhou198 <47863455+Zhou198@users.noreply.github.com>
2020-02-17 14:34:12 -0500

    Update 557.R

--- 557.R
+++ 557.R
@@ -4,18 +4,20 @@ library(survival)
 
 #### T1 ####
 set.seed(1)
-Y <- rweibull(100, 2, 1)  
-U <- runif(100)
+Y <- rweibull(100, 2, 1); U <- runif(100, 0, 2)
+d <- as.numeric(Y <= U)
 M <- Y * (Y <= U) + U * (Y > U)
-  
 
+sum(d)
 
 #### T2 ####
-set.seed(1)
-Y <- rweibull(100, 0.5, 1)
-U <- runif(100, 0, 2)
-d <- as.numeric(Y <= U)
-M <- Y * d + U * (1 - d)
+# set.seed(1)
+# Y <- rweibull(100, 0.5, 1); U <- runif(100, 0, 2)
+# d <- as.numeric(Y <= U)
+# M <- Y * d + U * (1 - d)
+# 
+# sum(d)
+
 
 model <- survreg(Surv(M, d) ~ 1)
 val <- summary(model)
@@ -36,7 +38,7 @@ der1 <- function(ka, ta, x, d) {
            -ka/ta * sum(d) + ka/ta * sum(xdt^ka)), ncol = 1)
 }
 
-KT <- c(1, 2); S <- c(0, 0); 
+KT <- c(1, 1); S <- c(0, 0) 
 while (max(abs(KT - S)) >= 0.00001) {
   S <- KT; inv <- solve(der2(KT[1], KT[2], M, d), diag(2))
   KT <- KT - inv %*% der1(KT[1], KT[2], M, d)
@@ -44,7 +46,7 @@ while (max(abs(KT - S)) >= 0.00001) {
 
 c(alpha = log(KT[2]), logsigma = -log(KT[1]), sigma = 1/KT[1])
 
-KT
+KT <- c(1/val$scale, exp(val$coef))
 
 
 ####### T3 #######
@@ -62,10 +64,10 @@ vrho <- t(c(0, -1/(KT[2]^2))) %*% -solve(der2(KT[1], KT[2], M, d), diag(2)) %*% 
 
 
 #### T5 ####
-set.seed(1)
-Y <- rweibull(100, 2, 1); U <- runif(100)
-d <- as.numeric(Y <= U) 
-M <- Y * d + U * (1 - d)
+# set.seed(1)
+# Y <- rweibull(100, 2, 1); U <- runif(100)
+# d <- as.numeric(Y <= U) 
+# M <- Y * d + U * (1 - d)
 
 model5 <-  survreg(Surv(M, d) ~ 1)
 est <- data.frame(ka1 = 1/model5$scale, ta1 = exp(model5$coef), ta0 = mean(M)/mean(d))
@@ -74,17 +76,23 @@ est <- data.frame(ka1 = 1/model5$scale, ta1 = exp(model5$coef), ta0 = mean(M)/me
 survreg(Surv(M, d) ~ 1, dist = "exponential")
 
 
-2 * ((log(est[1] * est[3]) - est[1] * log(est[2])) * sum(d) + (est[1] - 1) * sum(d * log(M)) + sum(M)/est[3] - est[2]^(-est[1]) * sum(M^est[1]))
+2 * ((log(est[1] * est[3]) - est[1] * log(est[2])) * sum(d) + (est[1] - 1) * sum(d * log(M)) + sum(M)/est[3] - sum((M/est[2])^est[1]))
 
 qchisq(0.95, df = 1)
 
-  
+
 
 #### T7 ####
 set.seed(1)
 Y <- rweibull(4, 2, 1); U <- runif(4)
 d <- as.numeric(Y <= U) 
 M <- Y * d + U * (1 - d)
+
+
+set.seed(1)
+Y <- rweibull(4, 2, 1); U <- runif(4, 0, 2)
+d <- as.numeric(Y <= U)
+M <- Y * (Y <= U) + U * (Y > U)
 
 model5 <-  survreg(Surv(M, d) ~ 1)
 est <- data.frame(ka1 = 1/model5$scale, ta1 = exp(model5$coef), ta0 = mean(M)/mean(d))
